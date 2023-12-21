703fcd8ef5d4012f31587f6deeabc5dfa63dc84e
Łukasz Jarzęcki <jarzeckil@gmail.com>
2023-12-21 19:04:52 +0100

    implementacja eliminacji gaussa

--- gauss.c
+++ gauss.c
@@ -1,3 +1,4 @@
+//łukasz jarzęcki
 #include "gauss.h"
 
 /**
@@ -5,10 +6,30 @@
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
 int eliminate(Matrix *mat, Matrix *b){
-    /**
-  	 * Tutaj należy umieścić właściwą implemntację.
-		 */
+    
 
+	//sprawdzenie osobliwosci
+	for (int i = 0; i < mat->c; i++)
+	{
+		if (mat->data[i][i] == 0)
+			return 1;
+	}
+
+
+	//eliminacja
+	for (int i = 0; i < mat->c-1; i++)
+	{
+		for (int j = i+1; j < mat->r; j++) 
+		{
+			double m = mat->data[j][i]/ mat->data[i][i];
+			for (int h = 0; h < mat->c; h++)
+			{
+				mat->data[j][h] -= m * mat->data[i][h];
+			}
+			b->data[j][0] -= m * b->data[i][0];
+
+		}
+	}
 		return 0;
 }
 
