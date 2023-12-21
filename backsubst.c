2ff983f2a96b0119c6d6a5998e122264ba4e51b8
pskier <153275518+pskier@users.noreply.github.com>
2023-12-21 13:42:07 +0100

    return 2

--- backsubst.c
+++ backsubst.c
@@ -6,18 +6,19 @@
  * Zwraca 2 - błąd nieprawidłowych rozmiarów macierzy
  */
 int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
-				/**
-				 * Tutaj należy umieścić właściwą implemntację.
-				 */
+	int a = mat->r -1;
+	int b=mat->c-1;
 
-				/* To ponizej jest przepisaniem b do x. Nalezy to poprawic! */
+	if(a!=b)
+		return 2;//zwraca 2 dla nieodpowiedniego rozmiaru macierzy
+	
 
-				int i;
-				for (i =0; i < x->r; i++) {
-								x->data[i][0] = b->data[i][0];
-				}
+	int i;
+	for (i =0; i < x->r; i++) {
+		x->data[i][0] = b->data[i][0];
+	}
 
-				return 0;
+return 0;
 }
 
 
